9d43e00cda9f7b633096dc80cb9cd2d080169b2c
uni-mbing <max.bing.mb@gmail.com>
2026-01-05 19:59:01 +0100

    improve buffer creation

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,7 @@ int main(int argc, char** argv){
 
 	const int width = 400;
 	const int height = 400;
-	std::pair<Vec3D, Color> test1, test2, test3, test4, test5;
-	test1.first = Vec3D{width/2.0, height/ 2.0, 0};
-	test1.second = Color{0, 255, 0, 255};
-	test2.first = Vec3D{width, height, 0};
-	test2.second = Color{255, 0, 255, 255};
-	test3.first = Vec3D{0, 0, 0};
-	test3.second = Color{0, 0, 255, 255};
-	test4.first = Vec3D{width, 0, 0};
-	test4.second = Color{255, 0, 0, 255};
 
-	// std::vector<std::pair<Vec3D, Color>> buffer = {test1, test2, test3, test4, test5};//(width * height * 1, std::pair<Vec3D, Color>({0,0,0}, {0,0,0,0}));
 	std::vector<std::pair<Vec3D, Color>> buffer;
 
 	createCircleBuffer(buffer, width, height);
